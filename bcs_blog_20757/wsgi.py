56937307d176cb3fbd118aa3f50830d63924ab01
Crowdbotics <team@crowdbotics.com>
2020-09-27 16:18:47 +0000

    Update models

--- bcs_blog_20757/wsgi.py
+++ bcs_blog_20757/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bcs_blog_20757.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bcs_blog_20757.settings")
 
 application = get_wsgi_application()
